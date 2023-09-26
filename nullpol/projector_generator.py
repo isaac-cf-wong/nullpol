2acf87a8bf5036c19587e06e9f44d163c1927805
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2023-09-26 17:09:37 +0800

    Naming Convention

--- nullpol/projector_generator.py
+++ nullpol/projector_generator.py
@@ -12,7 +12,7 @@ polarization_dict = {
 }
 
 
-class projector_generator(object):
+class ProjectorGenerator(object):
     """Null projector generator."""
 
     def __init__(self, parameters=None, waveform_arguments=None):
@@ -24,7 +24,6 @@ class projector_generator(object):
 
         self.basis_str = waveform_arguments['basis']
         self.basis = np.sum([polarization_dict[k] for k in self.basis_str], axis=0, dtype=bool)
-        self.amp_phase_factor = waveform_arguments['amp_phase_factor']
 
         self.interferometers = waveform_arguments['interferometers']
         self.detect_names = [interferometer.name for interferometer in self.interferometers]
@@ -47,7 +46,7 @@ class projector_generator(object):
         """
         antenna_pattern_matrix = (antenna_pattern.get_antenna_pattern_matrix(self.detect_names, parameters['ra'], parameters['dec'], parameters['psi'], parameters['geocent_time'], self.polarization))
         whitened_antenna_pattern_matrix = antenna_pattern.whiten_antenna_pattern_matrix(antenna_pattern_matrix, self.frequency_array, self.psd_array)
-        whitened_antenna_pattern_matrix_new_basis = antenna_pattern.change_basis(whitened_antenna_pattern_matrix, self.basis, self.amp_phase_factor)
+        whitened_antenna_pattern_matrix_new_basis = antenna_pattern.change_basis(whitened_antenna_pattern_matrix, self.basis, parameters['amp_phase_factor'])
 
         return null_projector.get_null_projector(whitened_antenna_pattern_matrix_new_basis)
     
