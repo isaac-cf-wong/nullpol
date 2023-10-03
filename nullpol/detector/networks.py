222dfd0a854dc3c28ee45db3412724c3b31748f3
Isaac Wong <isaacwong@Isaacs-MacBook-Air.local>
2023-10-03 14:49:00 +0800

    updated networks.py

--- nullpol/detector/networks.py
+++ nullpol/detector/networks.py
@@ -10,7 +10,7 @@ def time_domain_strain_array(self):
         self._time_domain_strain_array = np.zeros((nifo, nfreq), dtype=self[0].time_domain_strain[0].dtype)
 
         for i in range(nifo):
-            self._time_domain_strain_array[i,:] = self[0].time_domain_strain            
+            self._time_domain_strain_array[i,:] = self[i].time_domain_strain            
 
     return self._time_domain_strain_array
 
@@ -22,15 +22,25 @@ def frequency_domain_strain_array(self):
         self._frequency_domain_strain_array = np.zeros((nifo, nfreq), dtype=self[0].frequency_domain_strain[0].dtype)
 
         for i in range(nifo):
-            self._frequency_domain_strain_array[i,:] = self[0].frequency_domain_strain            
+            self._frequency_domain_strain_array[i,:] = self[i].frequency_domain_strain            
 
     return self._frequency_domain_strain_array
 
 @property
 def time_frequency_domain_strain_array(self):
-    raise NotImplementedError
+    if self._frequency_domain_strain_array is None:
+        nifo = len(self)
+        ntime, nfreq = self[0].time_frequency_domain_strain.shape
+        self._frequency_domain_strain_array = np.zeros((nifo, ntime, nfreq), dtype=self[0].time_frequency_domain_strain[0, 0].dtype)
+
+        for i in range(nifo):
+            self._time_frequency_domain_strain_array[i,:] = self[i].time_frequency_domain_strain
+
+    return self._time_frequency_domain_strain_array
 
 bilby.gw.detector.InterferometerList._time_domain_strain_array = None
 bilby.gw.detector.InterferometerList.time_domain_strain_array = time_domain_strain_array
 bilby.gw.detector.InterferometerList._frequency_domain_strain_array = None
 bilby.gw.detector.InterferometerList.frequency_domain_strain_array = frequency_domain_strain_array
+bilby.gw.detector.InterferometerList._time_frequency_domain_strain_array = None
+bilby.gw.detector.InterferometerList.time_frequency_domain_strain_array = time_frequency_domain_strain_array
