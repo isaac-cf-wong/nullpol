652b25f67ab985a8e10fbef10dfab32f38d44fbb
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2023-11-02 11:18:54 +0800

    Update networks.py

--- nullpol/detector/networks.py
+++ nullpol/detector/networks.py
@@ -1,6 +1,7 @@
 import numpy as np
 import bilby.gw.detector
-
+from bilby.core.utils import logger
+import math
 
 @property
 def time_domain_strain_array(self):
@@ -52,6 +53,40 @@ def whitened_frequency_domain_strain_array(self):
 
     return self._whitened_frequency_domain_strain_array
 
+def _check_interferometers(self):
+    """Verify IFOs 'duration', 'sampling_frequency' are the same.
+
+    If the above attributes are not the same, then the attributes are checked to
+    see if they are the same up to 5 decimal places.
+
+    If both checks fail, then a ValueError is raised.
+    """
+    consistent_attributes = ["duration", "sampling_frequency"]
+    for attribute in consistent_attributes:
+        x = [
+            getattr(interferometer.strain_data, attribute)
+            for interferometer in self
+        ]
+        try:
+            if not all(y == x[0] for y in x):
+                ifo_strs = [
+                    "{ifo}[{attribute}]={value}".format(
+                        ifo=ifo.name,
+                        attribute=attribute,
+                        value=getattr(ifo.strain_data, attribute),
+                    )
+                    for ifo in self
+                ]
+                raise ValueError(
+                    "The {} of all interferometers are not the same: {}".format(
+                        attribute, ", ".join(ifo_strs)
+                    )
+                )
+        except ValueError as e:
+            if not all(math.isclose(y, x[0], abs_tol=1e-5) for y in x):
+                raise ValueError(e)
+            else:
+                logger.warning(e)
 
 bilby.gw.detector.InterferometerList._time_domain_strain_array = None
 bilby.gw.detector.InterferometerList.time_domain_strain_array = time_domain_strain_array
@@ -61,3 +96,4 @@ bilby.gw.detector.InterferometerList._time_frequency_domain_strain_array = None
 bilby.gw.detector.InterferometerList.time_frequency_domain_strain_array = time_frequency_domain_strain_array
 bilby.gw.detector.InterferometerList._whitened_frequency_domain_strain_array = None
 bilby.gw.detector.InterferometerList.whitened_frequency_domain_strain_array = whitened_frequency_domain_strain_array
+bilby.gw.detector.InterferometerList._check_interferometers = _check_interferometers
