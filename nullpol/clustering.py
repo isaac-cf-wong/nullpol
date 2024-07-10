2b1c56ba42927929439c063060540e8fe2ac43d1
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-07-10 10:20:21 +0800

    Add padding

--- nullpol/clustering.py
+++ nullpol/clustering.py
@@ -26,16 +26,24 @@ def _dfs(i, j, mask, visited):
                 stack.append(neighbour)
     return cluster
 
-def clustering(time_freq_transformed, threshold=90):
+def clustering(time_freq_transformed, dt, df, padding_time=0.1, padding_freq=10, threshold=90):
     """
-    Clusters the time-frequency transformed data and returns a mask with the largest cluster.
+    Filter the time-frequency transformed data with a threshold and return the largest cluster with a padding.
 
     Parameters
     ----------
     time_freq_transformed : numpy.ndarray
         Time-frequency transformed data in shape (n_time, n_freq).
-    threshold : int, optional
-        The threshold for the clustering. Default is 10.
+    threshold : float, optional
+        The threshold for the filtering. Default is 90.
+    dt : float
+        The time resolution in seconds.
+    df : float
+        The frequency resolution in Hz.
+    padding_time : float, optional
+        The padding in time in seconds. Default is 0.1.
+    padding_freq : float, optional
+        The padding in frequency in Hz. Default is 10.
 
     Returns
     -------
@@ -43,19 +51,29 @@ def clustering(time_freq_transformed, threshold=90):
         A mask with the largest cluster in shape (n_time, n_freq).
     """
     # threshold the data
-    mask = time_freq_transformed > np.percentile(time_freq_transformed, threshold)
+    filter = time_freq_transformed > np.percentile(time_freq_transformed, threshold)
 
     # find clusters
-    visited = np.zeros(mask.shape, dtype=np.uint8)
+    visited = np.zeros(filter.shape, dtype=np.uint8)
     clusters = []
-    for i in range(mask.shape[0]):
-        for j in range(mask.shape[1]):
-            if mask[i, j] and not visited[i, j]:
-                clusters.append(_dfs(i, j, mask, visited))
+    for i in range(filter.shape[0]):
+        for j in range(filter.shape[1]):
+            if filter[i, j] and not visited[i, j]:
+                clusters.append(_dfs(i, j, filter, visited))
 
-    # return a mask with the largest cluster
+    # find the largest cluster
     largest_cluster = max(clusters, key=len)
-    mask = np.zeros(mask.shape, dtype=np.uint8)
+    mask = np.zeros(filter.shape, dtype=np.uint8)
     for i, j in largest_cluster:
         mask[i, j] = 1
+
+    # add padding
+    padding_time = int(np.ceil(padding_time / dt))
+    padding_freq = int(np.ceil(padding_freq / df))
+    for i, j in largest_cluster:
+        for x in range(-padding_time, padding_time + 1):
+            for y in range(-padding_freq, padding_freq + 1):
+                if 0 <= i + x < mask.shape[0] and 0 <= j + y < mask.shape[1]:
+                    mask[i + x, j + y] = 1
+    
     return mask
