5a350fd8123809533cf05f21a221fc70795a947c
Isaac Wong <chun-fung.wong@tgfli-s0.phy.cuhk.edu.hk>
2024-08-09 14:25:15 +0800

    added clustering and TF transform interface

--- nullpol/detector/interferometer.py
+++ nullpol/detector/interferometer.py
@@ -1,17 +1,8 @@
-from bilby.gw.detector.interferometer import (
-    Interferometer as BilbyInterferometer
-)
-from bilby.gw.detector.calibration import Recalibrate
+import bilby
+from bilby.core.utils.introspection import PropertyAccessor
 
 
-class Interferometer(BilbyInterferometer):
-    """A class to represent a single interferometer
-    """
-    def __init__(self, name, power_spectral_density, minimum_frequency, maximum_frequency, length, latitude, longitude,
-                 elevation, xarm_azimuth, yarm_azimuth, xarm_tilt=0., yarm_tilt=0., calibration_model=Recalibrate()):
-        super(Interferometer, self).__init__(name=name, power_spectral_density=power_spectral_density,
-                                             minimum_frequency=minimum_frequency, maximum_frequency=maximum_frequency,
-                                             length=length, latitude=latitude, longitude=longitude,
-                                             elevation=elevation, xarm_azimuth=xarm_azimuth,
-                                             yarm_azimuth=yarm_azimuth, xarm_tilt=xarm_tilt, yarm_tilt=yarm_tilt,
-                                             calibration_model=calibration_model)
+bilby.gw.detector.interferometer.Interferometer.whitened_time_frequency_domain_strain_array = PropertyAccessor('strain_data', 'whitened_time_frequency_domain_strain_array')
+bilby.gw.detector.interferometer.Interferometer.whitened_time_frequency_domain_quadrature_strain_array = PropertyAccessor('strain_data', 'whitened_time_frequency_domain_quadrature_strain_array')
+bilby.gw.detector.interferometer.Interferometer.Nf = PropertyAccessor('strain_data', 'Nf')
+bilby.gw.detector.interferometer.Interferometer.Nt = PropertyAccessor('strain_data', 'Nt')
--- nullpol/detector/strain_data.py
+++ nullpol/detector/strain_data.py
@@ -1,44 +1,69 @@
 import numpy as np
-from bilby.gw.detector.strain_data import (
-    InterferometerStrainData as BilbyInterferometerStrainData
-)
+import bilby
+from null.wdm.wavelet_transform import (transform_wavelet_freq,
+                                        transform_wavelet_freq_quadrature)
 
+@property
+def time_frequency_bandwidth(self):
+    return self._time_frequency_bandwidth
 
-class InterferometerStrainData(BilbyInterferometerStrainData):
-    """ Strain data for an interferometer """
-    def __init__(self, minimum_frequency=0., maximum_frequency=np.inf,
-                 roll_off=0.2, notch_list=None):
-        """Initiate an InterferometerStrainData object
+@time_frequency_bandwidth.setter
+def time_frequency_bandwidth(self, value):
+    self._time_frequency_bandwidth = value
+    self._Nf = int(self.sampling_frequency / 2 / value)
+    self._Nt = int(len(self) / self._Nf)
 
-        The initialized object contains no data, this should be added using one
-        of the `set_from..` methods.
+@property
+def Nf(self):
+    if self._Nf is None:
+        raise ValueError('Nf is undefined.')
+    return self._Nf
 
-        Parameters
-        ==========
-        minimum_frequency: float
-            Minimum frequency to analyze for detector. Default to 0.
-        maximum_frequency: float
-            Maximum frequency to analyze for detector. Default to np.inf.
-        roll_off: float
-            The roll-off (in seconds) used in the Tukey window, default=0.2s.
-            This corresponds to alpha * duration / 2 for scipy tukey window.
-        notch_list: bilby.gw.detector.strain_data.NotchList
-            A list of notches
-        """
-        super(InterferometerStrainData, self).__init__(
-            minimum_frequency=minimum_frequency,
-            maximum_frequency=maximum_frequency,
-            roll_off=roll_off,
-            notch_list=notch_list)
-        self._time_frequency_domain_strain = None
+@property
+def Nt(self):
+    if self._Nt is None:
+        raise ValueError('Nt is undefined.')
+    return self._Nt
 
-    @property
-    def time_frequency_domain_strain(self):
-        """ The time domain strain data
+@property
+def nx(self):
+    if self._nx is None:
+        raise ValueError('nx is undefined.')
+    return self._nx
 
-        Returns
-        =======
-        time_domain_strain: array_like
-            The time domain strain data
-        """
-        pass
+@nx.setter
+def nx(self, value):
+    self._nx = value
+
+@property
+def whitened_time_frequency_domain_strain_array(self):
+    if self._whitened_time_frequency_domain_strain_array is None:
+        # Perform time-frequency transform
+        self._whitened_time_frequency_domain_strain_array = transform_wavelet_freq(self.whitened_frequency_domain_strain_array,
+                                                                                   self.Nf,
+                                                                                   self.Nt,
+                                                                                   self.nx)
+    return self._whitened_time_frequency_domain_strain_array
+
+@property
+def whitened_time_frequency_domain_quadrature_strain_array(self):
+    if self._whitened_time_frequency_domain_quadrature_strain_array is None:
+        # Perform time-frequency transform
+        self._whitened_time_frequency_domain_quadrature_strain_array = transform_wavelet_freq_quadrature(self.whitened_frequency_domain_strain_array,
+                                                                                                         self.Nf,
+                                                                                                         self.Nt,
+                                                                                                         self.nx)
+    return self._whitened_time_frequency_domain_quadrature_strain_array
+
+bilby.gw.detector.strain_data.InterferometerStrainData._time_frequency_bandwidth = None
+bilby.gw.detector.strain_data.InterferometerStrainData.time_frequency_bandwidth = time_frequency_bandwidth
+bilby.gw.detector.strain_data.InterferometerStrainData._Nf = None
+bilby.gw.detector.strain_data.InterferometerStrainData._Nt = None
+bilby.gw.detector.strain_data.InterferometerStrainData._nx = None
+bilby.gw.detector.strain_data.InterferometerStrainData.Nf = Nf
+bilby.gw.detector.strain_data.InterferometerStrainData.Nt = Nt
+bilby.gw.detector.strain_data.InterferometerStrainData.nx = nx
+bilby.gw.detector.strain_data.InterferometerStrainData._whitened_time_frequency_domain_strain_array = None
+bilby.gw.detector.strain_data.InterferometerStrainData.whitened_time_frequency_domain_strain_array = whitened_time_frequency_domain_strain_array
+bilby.gw.detector.strain_data.InterferometerStrainData._whitened_time_frequency_domain_quadrature_strain_array = None
+bilby.gw.detector.strain_data.InterferometerStrainData.whitened_time_frequency_domain_quadrature_strain_array = whitened_time_frequency_domain_quadrature_strain_array
