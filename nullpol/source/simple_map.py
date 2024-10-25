9ce684760e56957a440b496951868d0589429fbf
Chun-Fung Wong <chun-fung.wong@ligo.org>
2024-10-24 22:56:25 -0700

    added arguments in non_gr simple_map to modify the amplitude of plus and cross modes

--- nullpol/source/simple_map.py
+++ nullpol/source/simple_map.py
@@ -4,7 +4,8 @@ from bilby.gw.source import _base_lal_cbc_fd_waveform
 def lal_binary_black_hole_non_gr_simple_map(
     frequency_array, mass_1, mass_2, luminosity_distance,
     a_1, tilt_1, phi_12, a_2, tilt_2, phi_jl,
-    theta_jn, phase, amp_x=0., amp_y=0., amp_b=0., amp_l=0.,
+    theta_jn, phase,
+    amp_plus=1., amp_cross=1., amp_x=0., amp_y=0., amp_b=0., amp_l=0.,
     **kwargs,
 ):
     waveform_kwargs = dict(
@@ -22,4 +23,6 @@ def lal_binary_black_hole_non_gr_simple_map(
     wf_dict['y'] = wf_dict['cross'] * amp_y
     wf_dict['breathing'] = wf_dict['plus'] * amp_b
     wf_dict['longitudinal'] = wf_dict['plus'] * amp_l
+    wf_dict['plus'] = wf_dict['plus'] * amp_plus
+    wf_dict['cross'] = wf_dict['cross'] * amp_cross
     return wf_dict
