28f8977202aca383b0fa9f11972634c1af33c8a7
Isaac C. F. Wong <chun-fung.wong@ligo.org>
2025-07-17 12:23:42 +0200

    feat: Add type hints in get_shape_of_wavelet_transform and get_shape_of_stft

--- src/nullpol/time_frequency_transform/helper.py
+++ src/nullpol/time_frequency_transform/helper.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 
-def get_shape_of_wavelet_transform(duration,
-                                   sampling_frequency,
-                                   wavelet_frequency_resolution):
+def get_shape_of_wavelet_transform(duration: float,
+                                   sampling_frequency: float,
+                                   wavelet_frequency_resolution: float) -> tuple[int, int]:
     """A helper function to get the shape of the wavelet transform.
 
     Args:
@@ -19,10 +19,20 @@ def get_shape_of_wavelet_transform(duration,
     return Nt, Nf
 
 
-def get_shape_of_stft(duration,
-                      sampling_frequency,
-                      frequency_resolution):
+def get_shape_of_stft(duration: float,
+                      sampling_frequency: float,
+                      frequency_resolution: float) -> tuple[int, int]:
+    """Get the shape of short-time Fourier transform.
+
+    Args:
+        duration (float): The duration of the data segment.
+        sampling_frequency (float): The sampling frequency of the data segment.
+        frequency_resolution (float): The frequency resolution of the short-time Fourier transform.
+
+    Returns:
+        Tuple[int, int]: The number of time and frequency bins in the wavelet transform (Nt, Nf).
+    """
     N = int(sampling_frequency / frequency_resolution)
     hop_size = N
-    num_segments = (duration*sampling_frequency - N) // hop_size + 1
+    num_segments = int(duration*sampling_frequency - N) // hop_size + 1
     return num_segments, N//2 + 1
