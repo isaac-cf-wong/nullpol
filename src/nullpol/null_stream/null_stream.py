638d7e6dce3908d453061c03b03073df4974b95c
Isaac C. F. Wong <chun-fung.wong@ligo.org>
2025-07-17 12:11:13 +0200

    refactor: Remove compute_whitened_frequency_domain_fractional_null_stream and add type hints

--- src/nullpol/null_stream/null_stream.py
+++ src/nullpol/null_stream/null_stream.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
 
-from numba import njit
+import numpy as np
 
 from .signal_estimator import estimate_frequency_domain_signal_at_geocenter
 from .time_shift import compute_time_shifted_frequency_domain_strain_array
 
 
-@njit
-def compute_whitened_frequency_domain_fractional_null_stream(
-    frequency_array,
-    frequency_mask,
-    whitened_frequency_domain_strain_array,
-    time_delay_array,
-    whitened_antenna_pattern_matrix,
-    fraction,
-):
+def compute_whitened_frequency_domain_null_stream(
+    frequency_array: np.ndarray,
+    frequency_mask: np.ndarray,
+    whitened_frequency_domain_strain_array: np.ndarray,
+    time_delay_array: np.ndarray,
+    whitened_antenna_pattern_matrix: np.ndarray,
+) -> np.ndarray:
     """Compute the whitened frequency domain fractional null stream.
 
     Args:
@@ -23,7 +21,6 @@ def compute_whitened_frequency_domain_fractional_null_stream(
         whitened_frequency_domain_strain_array (numpy.ndarray): 2D numpy array of whitened frequency domain strain.
         time_delay_array (numpy.ndarray): 1D numpy array of time delays.
         whitened_antenna_pattern_matrix (numpy.ndarray): 3D numpy array of whitened antenna pattern matrix.
-        fraction (float): Fraction of projection to the signal subspace.
 
     Returns:
         numpy.ndarray: 2D numpy array representing the fractional null stream.
@@ -36,42 +33,8 @@ def compute_whitened_frequency_domain_fractional_null_stream(
         time_delay_array=time_delay_array,
     )
     s_est = estimate_frequency_domain_signal_at_geocenter(
-        frequency_array=frequency_array,
         frequency_mask=frequency_mask,
-        whitened_frequency_domain_strain_array=whitened_frequency_domain_strain_array,
-        time_delay_array=time_delay_array,
+        whitened_frequency_domain_strain_array_at_geocenter=d_ts,
         whitened_antenna_pattern_matrix=whitened_antenna_pattern_matrix
     )
-    if fraction != 1.:
-        s_est = s_est * fraction
     return d_ts - s_est
-
-
-@njit
-def compute_whitened_frequency_domain_null_stream(
-    frequency_array,
-    frequency_mask,
-    whitened_frequency_domain_strain_array,
-    time_delay_array,
-    whitened_antenna_pattern_matrix,
-):
-    """Compute the whitened frequency domain null stream.
-
-    Args:
-        frequency_array (numpy.ndarray): 1D numpy array of frequencies.
-        frequency_mask (numpy.ndarray): 1D boolean numpy array for frequency mask.
-        whitened_frequency_domain_strain_array (numpy.ndarray): 2D numpy array of whitened frequency domain strain.
-        time_delay_array (numpy.ndarray): 1D numpy array of time delays.
-        whitened_antenna_pattern_matrix (numpy.ndarray): 3D numpy array of whitened antenna pattern matrix.
-
-    Returns:
-        numpy.ndarray: 2D numpy array representing the null stream.
-    """
-    return compute_whitened_frequency_domain_fractional_null_stream(
-        frequency_array=frequency_array,
-        frequency_mask=frequency_mask,
-        whitened_frequency_domain_strain_array=whitened_frequency_domain_strain_array,
-        time_delay_array=time_delay_array,
-        whitened_antenna_pattern_matrix=whitened_antenna_pattern_matrix,
-        fraction=1.,
-    )
