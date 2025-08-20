d69743b35c095ec23c5193ba454ed2fdd822033c
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-08-20 21:22:07 +0800

    refactor: remove unused likelihood classes and detector module

--- src/nullpol/analysis/likelihood/__init__.py
+++ src/nullpol/analysis/likelihood/__init__.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
 from .chi2_tf_likelihood import Chi2TimeFrequencyLikelihood
-from .fractional_tf_likelihood import FractionalProjectionTimeFrequencyLikelihood
-from .gaussian_tf_likelihood import GaussianTimeFrequencyLikelihood
 from .time_frequency_likelihood import TimeFrequencyLikelihood
 
 __all__ = [
     "Chi2TimeFrequencyLikelihood",
-    "FractionalProjectionTimeFrequencyLikelihood",
-    "GaussianTimeFrequencyLikelihood",
     "TimeFrequencyLikelihood",
 ]
