225ae4faba0edfbb853fdabfe33e2aaca03f55d1
Thomas Ng <thomas.ng@nikhef.nl>
2025-09-22 16:34:58 +0200

    refactor: remove unused config module and update tests for type conversion and error handling

--- src/nullpol/utils/__init__.py
+++ src/nullpol/utils/__init__.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-from .config import read_config
 from .convert_type import (
     convert_string_to_bool,
     convert_string_to_float,
     convert_string_to_int,
 )
 from .error import NullpolError
-from .filesystem import get_absolute_path, get_file_extension, is_file
+from .filesystem import get_file_extension, is_file
 from .log import logger, setup_logger
 
 __all__ = [
@@ -15,10 +14,8 @@ __all__ = [
     "convert_string_to_bool",
     "convert_string_to_float",
     "convert_string_to_int",
-    "get_absolute_path",
     "get_file_extension",
     "is_file",
     "logger",
-    "read_config",
     "setup_logger",
 ]
--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,10 +1,32 @@
 from __future__ import annotations
 
+from .test_convert_type import (
+    test_convert_string_to_bool,
+    test_convert_string_to_float,
+    test_convert_string_to_int,
+)
+from .test_error import (
+    test_nullpol_error_creation,
+    test_nullpol_error_inheritance,
+    test_nullpol_error_raising,
+)
 from .test_filesystem import test_get_file_extension, test_is_file
-from .test_log import test_setup_logger
+from .test_log import (
+    test_setup_logger_basic,
+    test_setup_logger_with_file_output,
+    test_setup_logger_with_log_level,
+)
 
 __all__ = [
+    "test_convert_string_to_bool",
+    "test_convert_string_to_float",
+    "test_convert_string_to_int",
     "test_get_file_extension",
     "test_is_file",
-    "test_setup_logger",
+    "test_nullpol_error_creation",
+    "test_nullpol_error_inheritance",
+    "test_nullpol_error_raising",
+    "test_setup_logger_basic",
+    "test_setup_logger_with_file_output",
+    "test_setup_logger_with_log_level",
 ]
